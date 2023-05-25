f6987ae631a903cbff505cb70bfd07774e28cdee
EADQ <easoftwareengineer@gmail.com>
2023-05-24 22:21:05 -0600

    trying to solve

--- split_a_string_in_balanced_strings/balanced_strings.py
+++ split_a_string_in_balanced_strings/balanced_strings.py
@@ -1,6 +1,20 @@
-class Solution(object):
-    def balancedStringSplit(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+# class Solution(object):
+    # def balancedStringSplit(self, s):
+        # """
+        # :type s: str
+        # :rtype: int
+        # """
+
+
+# r_count = s.count('R')
+# l_count = s.count('L')
+# output = 1
+# for i in s:
+    # if output == 1:
+        # output_1 = {r_count + l_count}
+        # if output == 2:
+            # output_2 = {}
+# print(lenght)
+s = 'RRLL'
+test = s.split(maxsplit=2)
+print(test)
