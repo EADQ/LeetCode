57ddb056dea61a7a23adb8e1d1e1bf84977b220f
EADQ <easoftwareengineer@gmail.com>
2023-05-31 22:53:03 -0600

    new try

--- split_a_string_in_balanced_strings/balanced_strings.py
+++ split_a_string_in_balanced_strings/balanced_strings.py
@@ -4,17 +4,34 @@
         # :type s: str
         # :rtype: int
         # """
+# def balancedStringSplit(s):
+    # balance = 0
+    # count = 0
+    # for i in s:
+        # if i == 'R':
+            # balance +=1
+        # else:
+            # balance -= 1
+
+        # if balance == 0:
+            # count +=1
+
+        # return count
+
+# s = ['RRLL']
+# test = balancedStringSplit(s)
+# print(test)
 
 
-# r_count = s.count('R')
-# l_count = s.count('L')
-# output = 1
-# for i in s:
-    # if output == 1:
-        # output_1 = {r_count + l_count}
-        # if output == 2:
-            # output_2 = {}
-# print(lenght)
-s = 'RRLL'
-test = s.split(maxsplit=2)
-print(test)
+def balanced_string_split(s):
+    balance = 0
+    indices = []
+    for i, char in enumerate(s):
+        if char == 'R':
+            balance += 1
+        else:  # char == 'L'
+            balance -= 1
+        if balance == 0:
+            indices.append(i)
+    substrings = [s[i:j+1] for i, j in zip([0]+indices, indices+[(len(s)-1)])]
+    return substrings
