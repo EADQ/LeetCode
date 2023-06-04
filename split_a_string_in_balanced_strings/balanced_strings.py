f8564075d96e5957651fec34249e070b1621518d
EADQ <easoftwareengineer@gmail.com>
2023-06-03 19:34:53 -0600

    final solution

--- split_a_string_in_balanced_strings/balanced_strings.py
+++ split_a_string_in_balanced_strings/balanced_strings.py
@@ -1,37 +1,11 @@
-# class Solution(object):
-    # def balancedStringSplit(self, s):
-        # """
-        # :type s: str
-        # :rtype: int
-        # """
-# def balancedStringSplit(s):
-    # balance = 0
-    # count = 0
-    # for i in s:
-        # if i == 'R':
-            # balance +=1
-        # else:
-            # balance -= 1
+def balancedStringSplit(self, s: str) -> int:
+    res = cnt = 0         
+    for c in s:
+        cnt += 1 if c == 'L' else -1            
+        if cnt == 0:
+            res += 1
+    return res  
 
-        # if balance == 0:
-            # count +=1
-
-        # return count
-
-# s = ['RRLL']
-# test = balancedStringSplit(s)
-# print(test)
-
-
-def balanced_string_split(s):
-    balance = 0
-    indices = []
-    for i, char in enumerate(s):
-        if char == 'R':
-            balance += 1
-        else:  # char == 'L'
-            balance -= 1
-        if balance == 0:
-            indices.append(i)
-    substrings = [s[i:j+1] for i, j in zip([0]+indices, indices+[(len(s)-1)])]
-    return substrings
+s = 'LLRR'
+test = balancedStringSplit(self,s)
+print(test)
